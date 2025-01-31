9437165339aa75346d3d3810cfdc5c346424079d
supriya31573 <goginenisupriya999@gmail.com>
2025-01-31 17:54:47 +0530

    Initial commit

---
+++ src/test/java/com/example/samplejavacicd/SamplejavacicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplejavacicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplejavacicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
